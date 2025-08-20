2bb656e91c7d17142aac9537ee2be6e02074064e
Shruthisq1 <shruthi.v@sq1.security>
2025-08-20 17:31:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,4 +5,16 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "terraform-sq1-012"
+    key          = "staging/terraform.tfstate"
+    region       = "ap-south-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
