bca70f85755da2e76e2844a033507dbcced4f1cd
Shruthisq1 <shruthi.v@sq1.security>
2025-08-20 15:30:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -82,39 +82,3 @@ resource "aws_iam_instance_profile" "admin_profile" {
   role = aws_iam_role.bastion_role.name
 
 }
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "terraform-s3"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
-
-  tags = {
-    Terraform   = "true"
-    Environment = "staging"
-  }
-}
-
-module "dynamodb_table" {
-  source = "terraform-aws-modules/dynamodb-table/aws"
-
-  name     = "terraform-locks"
-  hash_key = "LockID"
-
-  attributes = [
-    {
-      name = "LockID"
-      type = "S"
-    }
-  ]
-  tags = {
-    Terraform   = "true"
-    Environment = "staging"
-  }
-}
