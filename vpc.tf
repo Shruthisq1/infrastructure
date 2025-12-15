096c6aa8858b870fd2eade408300a4195051e1a8
Shruthisq1 <shruthi.v@sq1.security>
2025-12-15 12:11:09 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,25 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
+
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
 
   azs             = ["ap-south-2a", "ap-south-2b", "ap-south-2c"]
-  private_subnets = ["10.0.0.0/19","10.0.32.0/19","10.0.64.0/19"]
-  public_subnets  = ["10.0.96.0/19","10.0.128.0/19","10.0.160.0/19"]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+  public_subnets  = ["10.0.96.0/19", "10.0.128.0/19", "10.0.160.0/19"]
 
-  
-  enable_nat_gateway     = true
-  single_nat_gateway     = false              
-  one_nat_gateway_per_az = true              
-  reuse_nat_ips         = true
-  map_public_ip_on_launch = true           
-  
-  
-  external_nat_ip_ids = aws_eip.nat[*].id    
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  one_nat_gateway_per_az  = false
+  reuse_nat_ips           = true
+  map_public_ip_on_launch = true
 
-  enable_dns_hostnames   = true
-  enable_dns_support     = true
+  external_nat_ip_ids = aws_eip.nat[*].id
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Environment = var.environment
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
@@ -36,13 +36,12 @@ module "vpc" {
   }
 }
 
-
 resource "aws_eip" "nat" {
-  count = 3  
-  domain   = "vpc"
+  count  = 1
+  domain = "vpc"
 
   tags = {
-    Name        = "${var.cluster_name}-nat-eip-${count.index + 1}"
+    Name        = "${var.cluster_name}-nat-eip"
     Environment = var.environment
   }
 }
